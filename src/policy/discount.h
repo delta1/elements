7dde77697a32245a6e00aa5c88f58d46541b8506
Byron Hambly <bhambly@blockstream.com>
2024-02-06 15:51:28 +0200

    feat: discounted fees for confidential transactions
    This is a mempool policy only change to enable cheaper fees for
    Confidential Transactions.
    
    At a feerate of 1 sat/vb for a 2 input, 3 output transaction:
    - explicit tx fee 326 sats
    - confidential tx fee 437 sats

---
+++ src/policy/discount.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2021 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef ELEMENTS_POLICY_DISCOUNT_H
+#define ELEMENTS_POLICY_DISCOUNT_H
+
+#include <consensus/consensus.h>
+#include <cstdint>
+#include <primitives/transaction.h>
+#include <version.h>
+
+/**
+ * Calculate a smaller virtual size for discounted Confidential Transactions.
+ */
+static inline int64_t GetDiscountedVirtualTransactionSize(const CTransaction& tx)
+{
+    size_t weight = ::GetSerializeSize(tx, PROTOCOL_VERSION | SERIALIZE_TRANSACTION_NO_WITNESS) * (WITNESS_SCALE_FACTOR - 1) + ::GetSerializeSize(tx, PROTOCOL_VERSION);
+
+    // for each confidential output, subtract the output witness weight
+    for (size_t i = 0; i < tx.vout.size(); ++i) {
+        const CTxOut& output = tx.vout[i];
+        if (output.IsFee()) continue;
+        if (output.nAsset.IsCommitment() && output.nValue.IsCommitment()) {
+            weight -= ::GetSerializeSize(tx.witness.vtxoutwit[i], PROTOCOL_VERSION);
+        }
+    }
+    assert(weight > 0);
+
+    size_t discountvsize = (weight + WITNESS_SCALE_FACTOR - 1) / WITNESS_SCALE_FACTOR;
+
+    assert(discountvsize > 0);
+    return discountvsize;
+}
+
+#endif // ELEMENTS_POLICY_DISCOUNT_H
