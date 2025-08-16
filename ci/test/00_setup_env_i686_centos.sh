7f99afcad3e90c750dbd92491b1a4c79634f9498
Byron Hambly <bhambly@blockstream.com>
2025-08-16 10:05:26 +0200

    ci(rocky): set package_manager_install

--- ci/test/00_setup_env_i686_centos.sh
+++ ci/test/00_setup_env_i686_centos.sh
@@ -15,5 +15,3 @@ export GOAL="install"
 export NO_WERROR=1  # Suppress error: #warning _FORTIFY_SOURCE > 2 is treated like 2 on this platform [-Werror=cpp]
 export BITCOIN_CONFIG="--enable-zmq --with-gui=qt5 --enable-reduce-exports"
 export CONFIG_SHELL="/bin/dash"
-
-export CI_BASE_PACKAGES="gcc-c++ glibc-devel.x86_64 libstdc++-devel.x86_64 glibc-devel.i686 libstdc++-devel.i686 ccache libtool make git python3 python3-zmq which patch lbzip2 xz procps-ng dash rsync coreutils bison"
